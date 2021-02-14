8d38306e7b6b7801289d9dc268451eb5fe5d0c3c
Milan Shrestha <milanshrestha17@hotmail.com>
2021-02-14 13:50:10 -0500

    Added contact section to index page

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ function submitForm() {
     form.reset();  // Reset all form data
     return false; // Prevent page refresh
  }
+
+ function submitEmailForm() {
+   
+    let form = document.getElementById("email-form");
+    
+    form.submit(); // Submit the form
+    form.reset();  // Reset all form data
+    return false; // Prevent page refresh
+ }
